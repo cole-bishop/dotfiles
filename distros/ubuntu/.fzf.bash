89fe5f817f1d92a9347d59b0beb741c1239de24b
Cole <colewadebishop@gmail.com>
2019-04-23 21:13:58 -0500

    Added fzf changes.

--- distros/ubuntu/.fzf.bash
+++ distros/ubuntu/.fzf.bash
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/xloce/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/xloce/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/xloce/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/xloce/.fzf/shell/key-bindings.bash"
-
+source "$HOME/.fzf/shell/key-bindings.bash"
