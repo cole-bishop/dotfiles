e9f8ce9d4b61093e48d9f3755454389e0f51e778
Cole Bishop <cbishop@bushelpowered.com>
2019-12-20 05:04:24 +0000

    Migrated distro common elements to common folder.

---
+++ distros/common/home/.screenlayout/work-computer-pair-prog-north.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --auto
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 2240x1440 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x360 --rotate normal
